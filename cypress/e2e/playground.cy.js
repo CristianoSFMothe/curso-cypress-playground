5bdfcb1f34d9713bb8bb5e5bb2c7b787a69448ab
Cristiano Ferreira Mothe <cristianodevsystem@gmail.com>
2025-04-17 22:57:00 -0300

    refactor: Remove CI workflow file and update Cypress test cases for consistency

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -8,11 +8,18 @@ describe('Cypress Playground', () => {
     cy.get('#promotional-banner').should('be.visible')
   })
 
-  it.only('clicks the Subscribe button and show a success message', () => {
+  it('clicks the Subscribe button and show a success message', () => {
     cy.contains('button', 'Subscribe').should('be.visible').click()
 
     cy.contains('#success', "You've been successfully subscribed to our newsletter.")
       .should('be.visible')
   });
+
+  it('Types in an input which "signs" a form, the asserts it is signed', () => {
+    cy.get('#signature-textarea').should('be.visible').type('Cristiano')
+
+    cy.contains('#signature', 'Cristiano').should('be.visible')
+  });
+
 
 })
